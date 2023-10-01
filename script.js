8d5570e56f242d462dce6ae6441e53537785fe15
Shubham Singh <shubh4579@gmail.com>
2023-10-01 15:51:32 +0530

    added insta linkdin github contact form functional

--- script.js
+++ script.js
@@ -58,3 +58,21 @@ $(document).ready(function(){
         }
     });
 });
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyzHQui4pRXsHQcjMLLwv8r507gvfwj9J2ZBnJdkraEVaLXq1OIDBrL-wWYhYwzlai3iQ/exec'
+        const form = document.forms['submit-to-google-sheet']
+        const msg = document.getElementById("msg")
+      
+        form.addEventListener('submit', e => {
+          e.preventDefault()
+          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => {
+                msg.innerHTML = "Message Sent Successfully"
+                msg.style.opacity = 1;
+                setTimeout(function(){
+                    msg.innerHTML = ""
+                    msg.style.opacity = 0;
+                },3000)
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error.message))
+        })
